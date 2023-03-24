a4ddd15a90a4966eaf1d53e572985d415eb04ac7
JohnyDev <john.bedeir@gmail.com>
2023-03-24 07:53:33 +0100

    Dockerfile-docker-compose

--- main.go
+++ main.go
@@ -26,29 +26,68 @@ func main() {
 	// Set up MongoDB connection
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://db:27017")
 	client, _ = mongo.Connect(ctx, clientOptions)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", GetQuestion).Methods("GET")
 	router.HandleFunc("/", SubmitAnswer).Methods("POST")
+	router.HandleFunc("/newquestion", NewQuestion).Methods("GET")
+	router.HandleFunc("/newanswer", NewAnswer).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+// Question 1
 func GetQuestion(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode("What is your favorite programming language?")
 }
 
+// Question 2
+func NewQuestion(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("What is your favorite framework")
+}
+
+// Answer 1
 func SubmitAnswer(w http.ResponseWriter, r *http.Request) {
 	var answer Answer
 	_ = json.NewDecoder(r.Body).Decode(&answer)
+
+	log.Printf("answer.Answer_1: %v", answer.Answer_1)
+	log.Printf("answer.Answer_2: %v", answer.Answer_2)
+	log.Printf("answer.Answer_3: %v", answer.Answer_3)
 
 	collection := client.Database("surveyDB").Collection("answers")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	result, _ := collection.InsertOne(ctx, bson.M{"Answer1": answer.Answer_1, "Answer2": answer.Answer_2, "Answer3": answer.Answer_3})
+	result, err := collection.InsertOne(ctx, bson.M{"Answer1": answer.Answer_1, "Answer2": answer.Answer_2, "Answer3": answer.Answer_3})
+	if err != nil {
+		log.Fatalf("Error inserting answer: %v", err)
+	}
+
+	json.NewEncoder(w).Encode(result.InsertedID)
+}
+
+// Answer 2
+func NewAnswer(w http.ResponseWriter, r *http.Request) {
+	var answer Answer
+	_ = json.NewDecoder(r.Body).Decode(&answer)
+
+	log.Printf("answer.Answer_1: %v", answer.Answer_1)
+	log.Printf("answer.Answer_2: %v", answer.Answer_2)
+	log.Printf("answer.Answer_3: %v", answer.Answer_3)
+
+	collection := client.Database("surveyDB").Collection("answers")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := collection.InsertOne(ctx, bson.M{"Answer1": answer.Answer_1, "Answer2": answer.Answer_2, "Answer3": answer.Answer_3})
+	if err != nil {
+		log.Fatalf("Error inserting answer: %v", err)
+	}
+
 	json.NewEncoder(w).Encode(result.InsertedID)
 }
