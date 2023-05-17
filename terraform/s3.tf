7c1175af333f4037562d459bd073df4e42a72ba0
johnbedeir <john.bedeir@gmail.com>
2023-05-17 14:52:50 +0200

    tfdestroy-workflow-prevent-destroy-s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,22 +1,34 @@
-# resource "aws_s3_bucket" "s3" {
-#   bucket = "tfstate-comforte-prod"
+resource "aws_s3_bucket" "s3" {
+  bucket = "tfstate-comforte-prod"
 
-#   tags = {
-#     Environment = "production"
-#   }
-# }
+  tags = {
+    Environment = "production"
+  }
 
-# resource "aws_s3_bucket_ownership_controls" "s3_owner" {
-#   bucket = aws_s3_bucket.s3.id
-#   rule {
-#     object_ownership = "BucketOwnerPreferred"
-#   }
-# }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
-# resource "aws_s3_bucket_acl" "s3_acl" {
-#   depends_on = [aws_s3_bucket_ownership_controls.s3_owner]
+resource "aws_s3_bucket_ownership_controls" "s3_owner" {
+  bucket = aws_s3_bucket.s3.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 
-#   bucket = aws_s3_bucket.s3.id
-#   acl    = "private"
-# }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3_owner]
+
+  bucket = aws_s3_bucket.s3.id
+  acl    = "private"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
